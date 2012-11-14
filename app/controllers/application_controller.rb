a4f24e27293318d94b983cd531a9debbe4d8df99
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 21:08:38 +0800

    getting all issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
   end
+  before_filter :get_all_issues
+
+  def get_all_issues
+    @issues = Issue.all
+  end
 end
