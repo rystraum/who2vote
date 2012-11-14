11fcc8b43de13c9c6f6166aa7bf6f6ee1728c50b
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 19:33:36 +0800

    rails g model issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121114113236) do
+ActiveRecord::Schema.define(:version => 20121114113318) do
+
+  create_table "issues", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "politicians", :force => true do |t|
     t.string   "name"
