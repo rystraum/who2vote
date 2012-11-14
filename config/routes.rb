6ca0a7f16dd59fc0e999e615c5f3316377bea891
Rystraum Gamonez <rystraum@gmail.com>
2012-11-14 19:21:49 +0800

    rails g rails_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Who2vote::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  mount RailsAdmin::Engine => '/manage', :as => 'rails_admin'
+  devise_for :users
 
   root to: "home#index"
 end
